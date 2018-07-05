8479370f49cb5be8da466c9596f103426e1e4076
Apipol Sukgler <apipol@computerlogy.com>
2018-07-05 21:24:07 +0700

    STEP 3: Make GetCountries pass test

--- src/main.go
+++ src/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func GetCountries(w http.ResponseWriter, r *http.Request) {
-
+	c := []Country{
+		Country{Code: "PL", Name: "Poland", Capital: "Warsaw"},
+		Country{Code: "USA", Name: "USA", Capital: "Washington"},
+	}
+	json.NewEncoder(w).Encode(&c)
 }
